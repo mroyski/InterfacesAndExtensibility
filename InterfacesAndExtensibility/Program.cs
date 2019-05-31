8acfdf5fdb614ec3302a58562d0253f472bbfaf6
mroyski <mroyski@gmail.com>
2019-05-31 14:54:29 -0400

    created DbMigrator

--- InterfacesAndExtensibility/Program.cs
+++ InterfacesAndExtensibility/Program.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace InterfacesAndExtensibility
 {
+    public class DbMigrator
+    {
+        public void Migrate()
+        {
+            Console.WriteLine("Migration started at {0}", DateTime.Now);
+            Console.WriteLine("Migration finished at {0}", DateTime.Now);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
