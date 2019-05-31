0ddfea246cb7bee95eb40fdea73deae6ae86a760
mroyski <mroyski@gmail.com>
2019-05-31 14:57:19 -0400

    created ILogger and moved classes to individual files

---
+++ InterfacesAndExtensibility/DbMigrator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InterfacesAndExtensibility
+{
+    public class DbMigrator
+    {
+        public void Migrate()
+        {
+            Console.WriteLine("Migration started at {0}", DateTime.Now);
+            Console.WriteLine("Migration finished at {0}", DateTime.Now);
+        }
+    }
+}
--- InterfacesAndExtensibility/Program.cs
+++ InterfacesAndExtensibility/Program.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InterfacesAndExtensibility
 {
-    public class DbMigrator
-    {
-        public void Migrate()
-        {
-            Console.WriteLine("Migration started at {0}", DateTime.Now);
-            Console.WriteLine("Migration finished at {0}", DateTime.Now);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
